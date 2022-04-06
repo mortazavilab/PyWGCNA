94f17cb5447318c5e93e6582f093d570909f2e4d
Narges Rezaie <nargesr@uci.edu>
2022-04-06 15:01:24 -0700

    add option for analysing part

--- PyWGCNA/wgcna.py
+++ PyWGCNA/wgcna.py
@@ -410,7 +410,7 @@ class WGCNA(GeneExp):
                 else:
                     sys.exit("Given order is not valid!")
             for module in modules:
-                self.plotModuleEigenGene(module, metadata, show)
+                self.plotModuleEigenGene(module, metadata)
             print("\tDone..\n")
 
         if self.save:
@@ -2783,7 +2783,7 @@ class WGCNA(GeneExp):
             return None
         self.metadata_colors[col] = cmap
 
-    def plotModuleEigenGene(self, moduleName, metadata, show=True):
+    def plotModuleEigenGene(self, moduleName, metadata):
         """
         plot module eigen gene figure in given module
 
@@ -2791,8 +2791,6 @@ class WGCNA(GeneExp):
         :type moduleName: str
         :param metadata: list of metadata you want to be plotted
         :type metadata: list
-        :param show: indicate if you want to see plots in when you run your code
-        :type show: bool
         """
         sampleInfo = self.datExpr.obs
 
