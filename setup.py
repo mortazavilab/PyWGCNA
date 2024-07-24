b4c4d2777d8f506751f3834fe936b0277ecb9235
Narges Rezaie <nargesr@uci.edu>
2024-07-24 14:41:27 -0700

    update docs and tutorials fix #107, #108

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='PyWGCNA',  # the name of your package
     packages=['PyWGCNA'],  # same as above
-    version='2.0.4',  # version number
+    version='2.1.1',  # version number
     license='MIT',  # license type
     description='PyWGCNA is a Python package designed to do Weighted correlation network analysis (WGCNA)',
     # short description
     author='Narges Rezaie',  # your name
     author_email='nargesrezaie80@gmail.com',  # your email
     url='https://github.com/mortazavilab/PyWGCNA',  # url to your git repo
-    download_url='https://github.com/mortazavilab/PyWGCNA/archive/refs/tags/v2.0.4.zip',  # link to the tar.gz file associated with this release
+    download_url='https://github.com/mortazavilab/PyWGCNA/archive/refs/tags/v2.1.1.zip',  # link to the tar.gz file associated with this release
     keywords=['PyWGCNA', 'WGCNA', 'bulk', 'gene clustering', 'network analysis'],  #
     install_requires=[  # these can also include >, <, == to enforce version compatibility
         'pandas>=2.1.0',  # make sure the packages you put here are those NOT included in the
@@ -18,15 +18,15 @@ setup(
         'scipy>=1.9.1',
         'scikit-learn>=1.2.2',
         'statsmodels>=0.14.0',
-        'matplotlib>=3.5.2',
+        'matplotlib>=3.9.1',
         'seaborn>=0.11.2',
         'biomart>=0.9.2',
-        'gseapy>=1.0.1',
+        'gseapy>=1.1.3',
         'pyvis==0.3.1',
         'setuptools>=67.4.0',
         'biomart>=0.9.2',
         'reactome2py>=3.0.0',
-        'anndata>=0.8.0',
+        'anndata>=0.10.8',
         'requests>=2.28.1',
         'networkx>=2.8.4',
         'rsrc>=0.1.3',
